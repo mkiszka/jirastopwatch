64096ca99296ee0adda5d1a37994aef054bf2feb
Mateusz Kiszka <mateusz.kiszka@komlogo.pl>
2022-12-13 09:04:43 +0100

    remove test that is unnecessary
    - remove GetIssueSummary_OnFailure_It_Returns_Empty_String which is
    unnecessary since a0759f59ede1b8b6df2f9938eab9d75aedfd6574 commit

--- source/StopWatchTest/JiraClientTest.cs
+++ source/StopWatchTest/JiraClientTest.cs
@@ -107,30 +107,6 @@
             Assert.That(jiraClient.GetIssuesByJQL("testjql"), Is.Null);
         }
 
-
-        [Test, Description("GetIssueSummary: On success it returns a list of type filter")]
-        public void GetIssueSummary_OnSuccess_It_Returns_Issue_Summary()
-        {
-            Issue returnData = new Issue
-            {
-                Fields = new IssueFields
-                {
-                    Summary = "The long dark tea-time of the soul"
-                }
-            };
-
-            jiraApiRequesterMock.Setup(m => m.DoAuthenticatedRequest<Issue>(It.IsAny<IRestRequest>())).Returns(returnData);
-
-            Assert.That(jiraClient.GetIssueSummary("DG-42",false), Is.EqualTo(returnData.Fields.Summary));
-        }
-
-
-        [Test, Description("GetIssueSummary: On failure it returns empty string")]
-        public void GetIssueSummary_OnFailure_It_Returns_Empty_String()
-        {
-            //jiraApiRequesterMock.Setup(m => m.DoAuthenticatedRequest<Issue>(It.IsAny<IRestRequest>())).Throws<RequestDeniedException>();
-            jiraApiRequesterMock.Setup(m => m.DoAuthenticatedRequest<Issue>(It.IsAny<IRestRequest>())).Returns(new Issue() {Fields = new IssueFields() {Summary = string.Empty} });
-            Assert.That(jiraClient.GetIssueSummary("DG-42",false), Is.EqualTo(""));
         }
 
         [Test, Description("GetIssueTimetracking: On success it returns a timetracking object")]
